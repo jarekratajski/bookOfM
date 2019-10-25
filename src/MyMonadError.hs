4dfa5fc9af36e616f58853ad41413ee67aae1434
jarek ratajski <jratajski@gmail.com>
2019-10-25 11:40:21 -0700

    7.5 continue

--- src/MyMonadError.hs
+++ src/MyMonadError.hs
@@ -7,18 +7,7 @@ module MyMonadError where
 
 
 class (Monad m) => MonadError e m | m -> e where
-    -- | Is used within a monadic computation to begin exception processing.
     throwError :: e -> m a
-
-    {- |
-    A handler function to handle previous errors and return to normal execution.
-    A common idiom is:
-
-    > do { action1; action2; action3 } `catchError` handler
-
-    where the @action@ functions can call 'throwError'.
-    Note that @handler@ and the do-block must have the same return type.
-    -}
     catchError :: m a -> (e -> m a) -> m a
 
 instance MonadError e (Either e) where
