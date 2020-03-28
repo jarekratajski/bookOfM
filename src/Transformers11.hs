f977b53c2e4cceb362046db30e3ac0adb25bee71
jarek ratajski <jratajski@gmail.com>
2020-03-28 09:36:52 +0100

    10.2 compiles so it must be good

--- src/Transformers11.hs
+++ src/Transformers11.hs
@@ -17,6 +17,9 @@
 
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeOperators #-}
+
+{-# LANGUAGE TupleSections #-}
+
 module Transformers11 where
 
 import Control.Monad.Identity
@@ -73,10 +76,10 @@ instance Monad Evaluator where
 eval::Expr -> Evaluator Integer
 eval (Literal n ) = return n
 eval (Var v) = do
-                       a <- Evaluator $ fmap Just $ ask
-                       case lookup v a of
-                          Nothing -> evalFail
-                          Just v' ->  return v'
+  a <- Evaluator $ asks Just
+  case lookup v a of
+    Nothing -> evalFail
+    Just v' -> return v'
 eval (Op o x y) = do
                             u <- eval x
                             v <-eval y
@@ -95,26 +98,66 @@ evalFail = Evaluator $ ReaderT { runReaderT = \_ -> Identity Nothing}
 eval7 :: Expr -> MaybeT (State Assignment) Integer
 eval7 (Literal n ) = return n
 eval7 (Var v) = do
-                              a <-   get
-                              case lookup v a of
-                                  Nothing -> MaybeT { runMaybeT = state (\z -> (Nothing,z)) }
-                                  Just x -> return x
-                                 
+  a <- get
+  case lookup v a of
+    Nothing -> MaybeT {runMaybeT = state (Nothing, )}
+    Just x -> return x
+
 eval7 (Op o x y) = do
-                            u <- eval7 x
-                            v <-eval7 y
-                            case o of
-                                Add -> return (u + v)
-                                Subtract -> return (u - v)
-                                Multiply -> return (u * v)
-                                Divide -> if  v==0 then MaybeT { runMaybeT = state (\a -> (Nothing,a)) }
-                                                              else return (u `div` v)
-                                                              
-                                                              
+  u <- eval7 x
+  v <- eval7 y
+  case o of
+    Add -> return (u + v)
+    Subtract -> return (u - v)
+    Multiply -> return (u * v)
+    Divide ->
+      if v == 0
+        then MaybeT {runMaybeT = state (Nothing, )}
+        else return (u `div` v)
+
+
 evaltransformtest::IO ()
-evaltransformtest =  putStrLn $ show result
-                    where 
+evaltransformtest =  print result
+                    where
                         literal7 =  Literal 7
                         assignment = [("x", 4)]
-                        computation = (runState $ runMaybeT  $ eval7 literal7) assignment 
-                        result = show  <$> fst computation                                                                                                                     
+                        computation = (runState $ runMaybeT  $ eval7 literal7) assignment
+                        result = show  <$> fst computation
+
+
+
+-- monad instances
+data MyEitherT e m a = MyEitherT { runEitherT :: m (Either e a) }
+
+instance (Functor m) => Functor (MyEitherT e m) where
+     fmap f x = MyEitherT $ fmap (fmap f) (runEitherT x)
+
+instance (Applicative m) => Applicative (MyEitherT e m ) where
+    pure x = MyEitherT $ pure ( Right x)
+
+instance (Monad m) => Monad (MyEitherT e m ) where
+    return = pure
+    ( MyEitherT x )  >>= f = MyEitherT $ do
+                                            y <- x
+                                            case y of
+                                                Right r -> runEitherT $ f r
+                                                Left l -> return $ Left l
+
+newtype MyStateT s m a = MyStateT{runMyStateT :: s -> m (a, s)}
+
+instance (Functor m )=> Functor (MyStateT  s m ) where
+      fmap f x = MyStateT  func
+              where
+                  func y
+                    = let result = runMyStateT x y
+                          f1 (y1, s1) = (f y1, s1)
+                        in fmap f1 result
+
+
+instance (Applicative m) => Applicative (MyStateT s m) where
+      pure x = MyStateT $ \s -> fmap (\a -> (a,s)) (pure x) 
+
+-- testing functor
+-- let myMaybeA1 = Just 5
+
+
