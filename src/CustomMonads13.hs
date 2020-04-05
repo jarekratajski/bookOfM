136ebca02addc745ac5cf265edf5e849ee1c17b6
jarek ratajski <jratajski@gmail.com>
2020-04-05 11:15:34 +0200

    13.4 somehow done

--- src/CustomMonads13.hs
+++ src/CustomMonads13.hs
@@ -6,6 +6,9 @@ module CustomMonads13 where
 import Control.Monad.State.Lazy
 import Control.Monad.Trans.Reader
 import qualified Data.Map as   M( Map(..), lookup, insert)
+import System.Random (randomRIO)
+import qualified System.IO as System.IO
+import Control.Exception
 
 -- final style
 class Monad m => TicTacToe m where
@@ -47,4 +50,34 @@ instance TicTacToe (ReaderT Player (StateT Board IO)) where
                                         return NextTurn -- need to check state
                                         
                                         
-                    
+type FSError = IOError
+
+class FS m where
+  writeFileF :: FilePath -> String -> m (Either FSError ())
+  readFileF :: FilePath -> m (Either FSError String)
+
+instance FS IO where
+  writeFileF path contents = (Right <$> System.IO.writeFile path contents)
+                                            `catch` \ex -> return (Left ex)
+  readFileF path = (Right <$> System.IO.readFile path)
+                            `catch` \ex -> return (Left ex)
+
+
+--f:: (Monad m, FS m ) => FilePath -> m ( Either FSError() )
+--f path = do
+--          number <- randomRIO (1, 100)
+--          writeFileF path (show number)
+
+
+type MockFileSystem = M.Map FilePath String
+
+instance FS (State MockFileSystem) where
+    writeFileF path contents = do
+                              fs <- get
+                              put $ M.insert path contents fs
+                              return $ Right ()
+    readFileF path = do
+                                fs <- get
+                                return $ maybe nofile  Right $ M.lookup path fs
+                                where
+                                    nofile = Left $ userError "File does not exist"
